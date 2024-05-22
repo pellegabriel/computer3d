00d4f7f995ccfbf1ad6d827c3942cd4fd9448e52
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2024-05-22 16:14:04 -0300

    acomodando el deploy

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function HeroPage() {
   return (
-    <div className="relative bg-gray-50" style={{ width: '100%', height: '100%' }}>
+    <div
+      className="relative bg-gray-50"
+      style={{ width: "100%", height: "100%" }}>
       <iframe
+        title="UniqueTitleForIframe"
         src="https://polvorin01.netlify.app/"
         style={{
-          width: '100%',
-          height: '100%',
-          border: 'none',
+          width: "100%",
+          height: "100%",
+          border: "none",
         }}
       />
     </div>
