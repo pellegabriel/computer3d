40ca427a49dda796d397e4387bc6acb4f2d30d0d
pelleakronym <gpelle@akronym.cr>
2024-12-09 21:44:15 -0300

    cybertext animation

--- src/components/InfoText/CyberInfoText.jsx
+++ src/components/InfoText/CyberInfoText.jsx
@@ -1,35 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import '../../cyberpunk.css';
+import './CyberInfoText.css';
 
 const CyberInfoText = ({ title, description, details, isVisible }) => {
   const [glitching, setGlitching] = useState(false);
+  const [textVisible, setTextVisible] = useState(false);
+  const [currentText, setCurrentText] = useState('');
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const glitchInterval = setInterval(() => {
       setGlitching(true);
       setTimeout(() => setGlitching(false), 200);
-    }, 2000);
-    return () => clearInterval(interval);
+    }, 3000);
+
+    return () => clearInterval(glitchInterval);
   }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      setTextVisible(true);
+      let index = 0;
+      const textInterval = setInterval(() => {
+        if (index <= details.length) {
+          setCurrentText(details.slice(0, index));
+          index++;
+        } else {
+          clearInterval(textInterval);
+        }
+      }, 15);
+
+      return () => clearInterval(textInterval);
+    } else {
+      setTextVisible(false);
+      setCurrentText('');
+    }
+  }, [isVisible, details]);
 
   return (
     <div 
-    className="cyber-info-container cyber-razor-top cyber-razor-bottom bg-black"
-    style={{ 
-      opacity: isVisible ? 1 : 0,
-      padding: '1rem',
-      zIndex: 1002,
-      '--bg-opacity': '0.4',      
-    }}
-  >
-      <div className="cyber-tile cyber-glitch-2" >
-        <h2 className="cyber-h cyberpunk-font" style={{ marginTop: '0px' }}>
+      className="cyber-info-container cyber-razor-top cyber-razor-bottom bg-black"
+      style={{ 
+        padding: '1rem',
+        zIndex: 1002,
+        '--bg-opacity': '0.4',
+        position: 'absolute',
+        width: '100%',
+        opacity: isVisible ? 1 : 0,
+        transition: 'opacity 0.05s ease-out'
+      }}
+    >
+      <div 
+        className={`cyber-tile ${glitching ? 'cyber-glitch-2' : ''}`}
+        style={{
+          position: 'relative',
+          overflow: 'hidden'
+        }}
+      >
+        <h2 
+          className="cyber-h cyberpunk-font" 
+          style={{ 
+            marginTop: '0px'
+          }}
+        >
           {title}
         </h2>
       </div>
-      <div className="code-block bg-dark fg-white" data-title="DETAILS://" >
-        <div className="cyber-glitch-0">
-          {details}
+
+      <div 
+        className="code-block bg-dark fg-white" 
+        data-title="DETAILS://" 
+        style={{
+          position: 'relative',
+        }}
+      >
+        <div className={`cyber-text ${glitching ? 'cyber-glitch-0' : ''}`}>
+          {currentText}
+          <span className="cyber-cursor">_</span>
         </div>
       </div>
     </div>
